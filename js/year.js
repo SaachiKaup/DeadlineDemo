1de417079834f0786e4cc1c4cc5430454a065694
Saachi Kaup <saachi.kaup@gmail.com>
2022-06-09 15:01:20 +0530

    "Displays current year"

--- js/year.js
+++ js/year.js
@@ -1,1 +1,5 @@
 const copyrightSpan = document.querySelector('#___gatsby > div > div > section > div.fourstyle__FooterSection-sc-1qfzxsa-6.hWLxcw > div > p > span')
+
+let currentYear = new Date().getFullYear()
+
+copyrightSpan.innerHTML = `Copyright ${currentYear} by Restaverse. All rights reserved`
